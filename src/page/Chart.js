5228281bcc62c96a987c5400b7f45b24b5649f53
정근석 <jeong-geunseok@jeong-geunseog-ui-MacBookPro.local>
2020-11-20 13:54:07 +0900

    UPDATE chart.js

--- src/page/Chart.js
+++ src/page/Chart.js
@@ -1,23 +1,49 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { loadChart } from "../reducer/chart";
-import HighChart from "../component/HighChart";
+import { loadChart, loadChart2 } from "../reducer/chart";
+import EC_slab1 from "../component/EC_slab1";
+import EC_drain_PC from "../component/EC_drain_PC";
+import EC_slab2 from "../component/EC_slab2";
+import WC_slab1 from "../component/WC_slab1";
+import WC_slab2 from "../component/WC_slab2";
+import CO2air from "../component/CO2air";
+import Rhair from "../component/Rhair";
+import Tair from "../component/Tair";
+import EnScr from "../component/EnScr";
+import BlackScr from "../component/BlackScr";
+import HumDef from "../component/HumDef";
+import Iglob from "../component/Iglob";
+import RadSum from "../component/RadSum";
+import PipeGrow from "../component/PipeGrow";
+import PipeLow from "../component/PipeLow ";
+import Tout from "../component/Tout";
 
 const Chart = (props) => {
   const dispatch = useDispatch();
-  const chartList = useSelector((state) => state?.chart);
-  console.log("chartList", chartList);
-
-  const [chart, setChart] = useState("");
 
   useEffect(() => {
     dispatch(loadChart());
+    // dispatch(loadChart2());
   }, []);
 
   return (
     <React.Fragment>
-      <button onClick={() => setChart(false)}>dd</button>
-      <HighChart chart={true} />
+      <EC_drain_PC />
+      <EC_slab1 />
+      <EC_slab2 />
+      <WC_slab1 />
+      <WC_slab2 />
+      <CO2air />
+      <Rhair />
+      <Tair />
+      <EnScr />
+      <BlackScr />
+      <HumDef />
+      <Iglob />
+      <RadSum />
+      <PipeGrow />
+      <PipeLow />
+      <Tout />
     </React.Fragment>
   );
 };

c4c5e33b76725ed6dc7a8e72070aaefc1381e743
정근석 <jeong-geunseok@jeong-geunseog-ui-MacBookPro.local>
2020-11-20 13:54:37 +0900

    UPDATE reducer chart

--- src/reducer/chart.js
+++ src/reducer/chart.js
@@ -4,16 +4,32 @@ export const initialState = {
   loadChartLoading: false,
   loadChartDone: false,
   loadChartError: null,
+
+  loadChart2Loading: false,
+  loadChart2Done: false,
+  loadChart2Error: null,
 };
 
 export const LOAD_CHART_REQUEST = "LOAD_CHART_REQUEST";
 export const LOAD_CHART_SUCCESS = "LOAD_CHART_SUCCESS";
 export const LOAD_CHART_FAILURE = "LOAD_CHART_FAILURE";
 
+export const LOAD_CHART2_REQUEST = "LOAD_CHART2_REQUEST";
+export const LOAD_CHART2_SUCCESS = "LOAD_CHART2_SUCCESS";
+export const LOAD_CHART2_FAILURE = "LOAD_CHART2_FAILURE";
+
 export const loadChart = (data) => {
   console.log("loadChart: data", data);
   return {
     type: LOAD_CHART_REQUEST,
+    data,
+  };
+};
+
+export const loadChart2 = (data) => {
+  console.log("loadChart2: data", data);
+  return {
+    type: LOAD_CHART2_REQUEST,
     data,
   };
 };
@@ -42,6 +58,30 @@ const reducer = (state = initialState, action) => {
         ...state,
         loadChartLoading: false,
         loadChartError: action.error,
+      };
+
+    case LOAD_CHART2_REQUEST:
+      console.log("In REDUX, LOAD_CHART2_REQUEST, action : ", action);
+      return {
+        ...state,
+        loadChart2Loading: true,
+        loadChart2Done: false,
+        loadChart2Error: null,
+      };
+    case LOAD_CHART2_SUCCESS:
+      console.log("In REDUX, LOAD_CHART2_SUCCESS, action : ", action);
+      return {
+        ...state,
+        loadChart2Loading: false,
+        loadChart2Done: true,
+        chart: [action.data, ...state.chart],
+      };
+    case LOAD_CHART2_FAILURE:
+      console.log("In REDUX, LOAD_CHART2_FAILURE, action : ", action);
+      return {
+        ...state,
+        loadChart2Loading: false,
+        loadChart2Error: action.error,
       };
 
     default:
